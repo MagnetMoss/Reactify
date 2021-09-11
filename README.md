b974d8732dde57b0af40b7c77f744b5fece66e0a
Jaiar Rose <lfj143@gmail.com>
2021-09-11 00:14:17 -0700

    finished

--- README.md
+++
@@ -1,2 +0,0 @@
-# Reactify
-React Homework
---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Jared Rose",
+    "role": "Full Stack Web Developer(I Prefer Frontend)",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like spending my time dabbling in various parts of frontend development and like to learn about new technologies, or simply play video games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/jared-rose-099b7290/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/MagnetMoss",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://www.facebook.com/jared.rose.77770",
+          "className":"fa fa-facebook"
+        }
+      ],
+    "aboutme":" 'I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.' ~Unknown",
+    "address":"Washington State",
+    "website":"https://magnetmoss.github.io/Porfoliooo/",
+    "education":[
+      {
+        "UniversityName":"University of Washington",
+        "specialization":"Full Stack Web Development",
+        "MonthOfPassing":"Sep",
+        "YearOfPassing":"2021",
+        "Achievements":"Project 2: Best UI/UX"
+      },
+      {
+        "UniversityName":"Sozo School of Supernatural Ministry",
+        "specialization":"Theology/Religion",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2022",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Let's Settle It",
+        "description":"Conflict Resolution App",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"Forever Pig",
+        "description":"Baby Pig Leasing App",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
